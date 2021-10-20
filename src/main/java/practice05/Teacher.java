8a8d4e972c653814d7c0926a0f041032854cb612
Tommy Cheung <tommy200401@gmail.com>
2021-10-20 16:56:25 +0800

    [JavaOOP][Tommy] feat: updated practice 5

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,27 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        this(name, age, 0);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        if (getKlass() != 0){
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I teach Class %d.", getName(), getAge(), getKlass());}
+        else {
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I teach No Class.", getName(), getAge());
+        }
+    }
 }
